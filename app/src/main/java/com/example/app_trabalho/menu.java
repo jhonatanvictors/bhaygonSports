f2bd3855ec14a2286c8d005372c3da3d819180dc
Jhonatan Silva <jhonatanvictordasilva@gmail.com>
2019-11-23 23:23:09 -0300

    start commit

---
+++ app/src/main/java/com/example/app_trabalho/menu.java
@@ -0,0 +1,46 @@
+package com.example.app_trabalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        final Button jogos = (Button) findViewById(R.id.btnJogos);
+        jogos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jogosActivity();
+            }
+        });
+        final Button times = (Button) findViewById(R.id.btnTimes);
+        times.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timesActivity();
+            }
+        });
+        final Button competicao = (Button) findViewById(R.id.btnCompeticao);
+        competicao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                competicaoActivity();
+            }
+        });
+    }
+    private void jogosActivity(){startActivity(new Intent(menu.this, jogos.class));}
+    private void timesActivity(){startActivity(new Intent(menu.this, time_favorito.class));}
+    private void competicaoActivity(){startActivity(new Intent(menu.this, competicao.class));}
+}
+
+
+
+
